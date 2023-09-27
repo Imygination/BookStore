f64f8f00a3eed08a3651d9c6430030302626e974
agha david <aghadvid@gmail.com>
2023-09-28 04:04:08 +0700

    login.ejs

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const Controller = require('../controllers')
 
 router.get('/',Controller.home)
 router.use('/products',Controller )
+
+router.use('/products/add',Controller )//buat crud seller optional
+router.post('/products/add',Controller )
+
 router.use('/users',Controller )
 router.use('/transactions',Controller)
 
